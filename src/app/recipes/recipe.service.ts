063d3b15ca5f8d487390857fd298b1e6a3462811
SASIKANTBISWAL <biswalsashikant@gmail.com>
2020-02-25 14:51:47 +0530

    Changed the EventeEmitter with the Subject Observables.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -6,7 +6,6 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
         new Recipe('Chicken Biryani', 'A super-tasty chicken biryani - just awesome!', 'https://www.africanbites.com/wp-content/uploads/2018/04/IMG_0165.jpg', [
