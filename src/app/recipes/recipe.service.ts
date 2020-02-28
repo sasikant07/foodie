dbddcae0c2842621bb66113ccb5db4380552511c
SASIKANTBISWAL <biswalsashikant@gmail.com>
2020-02-28 21:56:07 +0530

    Added methods to the buttons to work appropiately.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -6,7 +7,8 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-    
+    recipeChanged = new Subject<Recipe[]>(); 
+
     private recipes: Recipe[] = [
         new Recipe('Chicken Biryani', 'A super-tasty chicken biryani - just awesome!', 'https://www.africanbites.com/wp-content/uploads/2018/04/IMG_0165.jpg', [
             new Ingredient('Basmati Rice', 1),
@@ -34,5 +36,20 @@ export class RecipeService {
 
       addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
+      }
+
+      addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipeChanged.next(this.recipes.slice());
+      }
+
+      updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipeChanged.next(this.recipes.slice());
+      }
+
+      deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipeChanged.next(this.recipes.slice());
       }
 }
