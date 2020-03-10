080d47a85e53e2f40cffffa7bf45627a29e0e4da
SASIKANTBISWAL <biswalsashikant@gmail.com>
2020-03-10 20:24:40 +0530

    Added a different spinner.

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-loading-spinner',
-    template: '<div class="lds-dual-ring"></div>',
+    template: '<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>',
     styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent {
